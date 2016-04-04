b48a1e99e5f0fe76595cb3d650481361cfc4dbfa
wbshi <1191007805@qq.com>
2016-04-05 02:06:59 +0800

    filter the contents with image in qiushibaike

--- PythonSpider/Exercises/qiushibaike.py
+++ PythonSpider/Exercises/qiushibaike.py
@@ -12,15 +12,16 @@ headers = {'User-Agent' : user_agent}
 try:
     request = urllib2.Request(url, headers=headers)
     response = urllib2.urlopen(request)
+    content = response.read().decode('utf-8')
+    pattern = re.compile('<div.*?author clearfix">.*?<a.*?<img.*?>(.*?)</a>.*?<div.*?' +
+                         'content">(.*?)<!--(.*?)-->.*?</div>(.*?)<div class="stats.*?class="number">(.*?)</i>', re.S)
+    items = re.findall(pattern, content)
+    for item in items:
+        haveImg = re.search("img", item[3])
+        if not haveImg:
+            print item[1]
 except urllib2.URLError, e:
     if hasattr(e, "code"):
         print e.code
     if hasattr(e, "reason"):
         print e.reason
-
-content = response.read().decode('utf-8')
-pattern = re.compile('<div class="content">(.*?)</div>', re.S)
-items = re.findall(pattern, content)
-print len(items)
-for item in items:
-    print items[0]
